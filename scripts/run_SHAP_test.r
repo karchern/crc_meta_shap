0798c11056aa4ceb1d6dc13b78346662575161c2
Nicolai Karcher <karchernic@gmail.com>
2024-03-22 14:02:55 +0100

    bringing profiles into right format allowing eval on both training and test set writing kernelshap object to disc for downstream eval

--- scripts/run_SHAP_test.r
+++ scripts/run_SHAP_test.r
@@ -12,63 +12,67 @@ if (interactive()) {
     args <- list()
     r_id <- 3
     f_id <- 5
+    on_what <- "training"
 } else {
     args <- commandArgs(trailingOnly = TRUE)
     r_id <- as.integer(args[1])
     f_id <- as.integer(args[2])
+    on_what <- args[3]
 }
 
 set.seed(11323)
 
-# profile <- read.table(here("data/Profiles.all.samples.tsv"), check.names = F)
-# meta <- read.table(here('data/Metadata.all.samples.tsv'), check.names = F) %>%
-#     rownames_to_column('sampleID') %>%
-#     filter(str_detect(Cohort, "Zeller")) %>%
-#     filter(Condition %in% c("CRC", "CTR"))
-
-model <- readRDS(here(str_c("data/models/model__resamp_id_", resamp_id, "__fold_id_", fold_id, ".rds")))
+# Load model
+model <- readRDS(here(str_c("data/models/model__resamp_id_", r_id, "__fold_id_", f_id, ".rds")))
+# Load profile
 profiles_all <- read.table(here("data/otu_table.tsv"))
+rownames(profiles_all) <- make.names(rownames(profiles_all))
+# subset to training and testing fold
 training_fold <- read_tsv(here('data/training_folds.tsv')) %>%
     filter(fold_id == f_id, resamp_id == r_id) %>%
     select(-fold_id, -resamp_id)
 testing_fold <- read_tsv(here('data/testing_folds.tsv')) %>%
     filter(fold_id == f_id, resamp_id == r_id) %>%
     select(-fold_id, -resamp_id)
+profiles_training <- profiles_all %>%
+    t() %>%
+    as.data.frame() %>%
+    rownames_to_column('sampleID') %>%
+    inner_join(training_fold) %>%
+    column_to_rownames('sampleID') %>%
+    # t() %>%
+    as.data.frame()
 profiles_testing <- profiles_all %>%
-    # filter(sampleID %in% testing_fold$sampleID)
-    inner_join()
+    t() %>%
+    as.data.frame() %>%
+    rownames_to_column('sampleID') %>%
+    inner_join(testing_fold) %>%
+    column_to_rownames('sampleID') %>%
+    # t() %>%
+    as.data.frame()
 
+if (on_what == "training") {
+    ps <- kernelshap(
+        model, X = profiles_training, bg_X = profiles_training[sample(nrow(profiles_training), 250), ],
+    )
+} else if (on_what == "testing") {
+    ps <- kernelshap(
+        model, X = profiles_testing, bg_X = profiles_testing[sample(nrow(profiles_testing), 250), ],
+    )
+} else {
+    stop("on_what must be either 'training' or 'testing'")
+}
 
-# profile <- profile[, colnames(profile) %in% meta$sampleID]
-# stopifnot(all(colnames(profile) == meta$sampleID))
-# profile <- profile[apply(profile, 1, function(x) max(x) > 0.01 && mean(x > 0) > 0.2), ]
-# profile <- log10(profile + 1E-5)
-# profileWithMeta <- profile %>%
-#     t() %>%
-#     as.data.frame() %>%
-#     rownames_to_column('sampleID') %>%
-#     left_join(meta[, c("sampleID", "Condition")], by = 'sampleID') %>%
-#     as.data.frame() %>%
-#     column_to_rownames('sampleID')
-# colnames(profileWithMeta) <- make.names(colnames(profileWithMeta))
-# tsk <- as_task_classif(profileWithMeta, target = "Condition", positive = "CRC")
+write_rds(ps, here(str_c("results/kernelshap_objects/resamp_id_", r_id, "__fold_id_", f_id, "__on_", on_what, ".rds")))
 
-# split = partition(tsk, ratio = 0.8)
-# learner = lrn("classif.ranger", predict_type = "prob")
-# learner$train(tsk, row_ids = split$train)
+# ps_shapviz <- shapviz(ps)
+# ps_shapviz_importance <- sv_importance(ps_shapviz, kind = 'both', max_display = 30)
+# sv_waterfall(ps_shapviz, row_id = 1)
 
-# This runs for a bit
-ps <- kernelshap(
-    model, X = training_fold, bg_X = testing_fold,
-)
-ps_shapviz <- shapviz(ps)
-ps_shapviz_importance <- sv_importance(ps_shapviz, kind = 'both', max_display = 30)
-sv_waterfall(ps_shapviz, row_id = 1)
-
-library(randomForest)
-rfOOBImpoortance <- randomForest(
-    as.formula(str_c("Condition ~ ", str_c(colnames(profileWithMeta)[colnames(profileWithMeta) != "Condition"], collapse = " + "))),
-    # as.formula(str_c("effect_size ~ ", str_c(colnames(trainData)[1:10000], collapse = " + "))),
-    data = profileWithMeta %>% mutate(Condition = factor(Condition, levels = c("CTR", "CRC"))),
-    importance = TRUE
-)$importance
+# library(randomForest)
+# rfOOBImpoortance <- randomForest(
+#     as.formula(str_c("Condition ~ ", str_c(colnames(profileWithMeta)[colnames(profileWithMeta) != "Condition"], collapse = " + "))),
+#     # as.formula(str_c("effect_size ~ ", str_c(colnames(trainData)[1:10000], collapse = " + "))),
+#     data = profileWithMeta %>% mutate(Condition = factor(Condition, levels = c("CTR", "CRC"))),
+#     importance = TRUE
+# )$importance
