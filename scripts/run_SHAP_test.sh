2442def6c67b208204b0be4818fc6bd16d022289
Nicolai Karcher <karchernic@gmail.com>
2024-04-10 09:03:58 +0200

    updating and finalizing slurm stuff

--- scripts/run_SHAP_test.sh
+++ scripts/run_SHAP_test.sh
@@ -3,8 +3,11 @@ do
 	for f_id in 1 2 3 4 5 6 7 8 9 10
 	do
 		for on_what in training testing
-		do 
-			echo -e "Rscript run_SHAP_test.r ${r_id} ${f_id} ${on_what}"
+			do
+			for which_model in RF lasso
+				do
+				echo -e "Rscript run_SHAP_test.r ${r_id} ${f_id} ${on_what} ${which_model}"
+			done
 		done
 	done
 done > iv.sh
