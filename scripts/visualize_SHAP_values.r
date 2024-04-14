a8683a5a97d3f85a230372fca5bc7bee1f4c020a
Nicolai Karcher <karchernic@gmail.com>
2024-04-14 10:11:05 +0200

    making analysis on TESTING data to conform to Quinten's data

--- scripts/visualize_SHAP_values.r
+++ scripts/visualize_SHAP_values.r
@@ -2,9 +2,15 @@
 library(tidyverse)
 library(here)
 library(ggembl)
+library(readxl)
+library(patchwork)
 
-dataset <- "Zeller"
-pc <- -4
+
+
+dataset <- "Quinten"
+# pc <- -4
+
+substrate_annotations <- read_xlsx("/g/scb/zeller/karcher/cazy_gut_microbiome/scripts/../data/20230607_glycan_annotations_cleaned.xlsx")
 
 # load models and clean up
 modelPaths <- list.files(here('data', 'models'), pattern = ".rds", full.names = TRUE)
@@ -87,7 +93,8 @@ shap <- shap %>%
 
 shap <- shap %>%
     mutate(shap_values_long = pmap(list(1:dim(shap)[1], shap_values, profile), \(i, x, p) {
-        tmp <- x$S$CRC %>% as.data.frame()
+        # tmp <- x$S$CRC %>% as.data.frame()
+        tmp <- x$S$`1` %>% as.data.frame()
         tmp$sampleID <- p$sampleID
         tmp <- tmp %>%
             pivot_longer(-sampleID) %>%
@@ -103,7 +110,7 @@ shap <- shap %>%
 
 
 # Boxplots of shap values
-shap_tmp <- shap %>%
+shap <- shap %>%
     select(fold, resampling, on, model_type, shap_values_long) %>%
     unnest(shap_values_long) %>%
     # I evalaute shap on training and testing
@@ -111,6 +118,8 @@ shap_tmp <- shap %>%
     # for training folds, I get  4 shap values (in 5x cv) per model and resampling
     # In any case, take the median shap value for each sampleID
     group_by(sampleID, on, model_type, feature)
+
+shap_tmp <- shap
 
 # Understand mean/variance relationship of test-sample shap values over resampling rounds
 
@@ -140,7 +149,8 @@ ggsave(plot = ggplot(data = lel,
 
 
 
-for (mt in c("RF", "lasso")) {
+# for (mt in c("RF", "lasso")) {
+for (mt in c("lasso")) {
     shap_tmp2 <- shap_tmp %>% filter(model_type == mt)
     shap_tmp2 <- shap_tmp2 %>%
         inner_join(shap_tmp2 %>%
@@ -169,7 +179,8 @@ for (mt in c("RF", "lasso")) {
 
 
 shap_tmp <- shap_tmp %>% summarize(shap_value = median(shap_value))
-for (mt in c("lasso", "RF")) {
+# for (mt in c("lasso", "RF")) {
+for (mt in c("lasso")) {
 
     shap_tmp2 <- shap_tmp %>%
         filter(model_type == mt)
@@ -231,9 +242,10 @@ for (mt in c("lasso", "RF")) {
 
 # For simplicity, let's move on with RF on testing data
 
-mt <- "RF"
+# mt <- "RF"
+mt <- "lasso"
 more_plot_data <- shap_tmp %>%
-    filter(model_type == mt, on == "training\nset")
+    filter(model_type == mt, on == "test\nset")
 
 more_plot_data$feature <- factor(more_plot_data$feature, levels = more_plot_data %>%
     group_by(feature) %>%
@@ -246,7 +258,7 @@ more_plot_data <- more_plot_data %>%
         group_by(feature) %>%
         summarize(n = mean(abs(shap_value))) %>%
         arrange(desc(n)) %>%
-        head(10), by = "feature")
+        head(25), by = "feature")
 l <- levels(more_plot_data$feature)
 
 more_plot_data <- more_plot_data %>%
@@ -256,7 +268,13 @@ more_plot_data <- more_plot_data %>%
             unnest() %>%
             distinct() %>%
             pivot_longer(-c(sampleID, Condition)) %>%
-            rename(feature = name, log10relAb = value), by = c('sampleID', "feature"))
+            rename(feature = name, feature_value = value), by = c('sampleID', "feature"))
+
+more_plot_data <- more_plot_data %>%
+    mutate(Condition = case_when(
+        Condition == "After fasting" ~ "After fasting",
+        Condition == "Before" ~ "Before fasting"
+    ))
 
 more_plot_data$feature <- factor(more_plot_data$feature, levels = l)
 
@@ -265,14 +283,15 @@ more_plot_data <- more_plot_data %>%
     left_join(more_plot_data %>%
         group_by(feature) %>%
         summarize(
-            spearman = cor(shap_value, log10relAb, method = "spearman")
+            spearman = cor(shap_value, feature_value, method = "spearman")
         ), by = 'feature') %>%
     mutate(spearman_sign = ifelse(spearman > 0, 1, -1))
 
 plot <- ggplot() +
     geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-    geom_point(data = more_plot_data %>% filter(log10relAb == pc), aes(x = feature, y = shap_value, shape = Condition), position = position_jitter(height = 0, width = 0.25), color = 'black', alpha = 0.35) +
-    geom_point(data = more_plot_data %>% filter(log10relAb > pc), aes(x = feature, y = shap_value, shape = Condition, color = log10relAb), position = position_jitter(height = 0, width = 0.25), alpha = 0.5) +
+    # geom_point(data = more_plot_data %>% filter(feature_value == pc), aes(x = feature, y = shap_value, shape = Condition), position = position_jitter(height = 0, width = 0.25), color = 'black', alpha = 0.35) +
+    # geom_point(data = more_plot_data %>% filter(feature_value > pc), aes(x = feature, y = shap_value, shape = Condition, color = feature_value), position = position_jitter(height = 0, width = 0.25), alpha = 0.5) +
+    geom_point(data = more_plot_data, aes(x = feature, y = shap_value, shape = Condition, color = feature_value), position = position_jitter(height = 0, width = 0.25), alpha = 0.5) +
     geom_point(data = more_plot_data %>%
         group_by(feature) %>%
         summarize(n = mean(abs(shap_value)) * spearman_sign), aes(x = feature, y = n), shape = 18, color = 'orange', size = 2.5, inherit.aes = F) +
@@ -283,35 +302,38 @@ plot <- ggplot() +
     coord_flip() +
     # ggtitle(str_c("Dataset: ", dataset, "\nModel: ", mt, "\neach dot is a sample\nblack dots samples\nwith feature == 0")) +
     scale_color_continuous(low = "blue", high = "yellow") +
-    scale_shape_manual(values = c("CRC" = 16, "CTR" = 1)) +
+    scale_shape_manual(values = c("After fasting" = 16, "Before fasting" = 1)) +
     NULL +
     ylab("SHAP value") +
     xlab("Genus")
 
 ggsave(plot = plot, filename = here('plots', str_c(dataset, "_SHAP_vs_relAb.pdf")), width = 5, height = 5)
 
-for (f in c("Fusobacterium", "Parvimonas", "Peptostreptococcus", "Porphyromonas", "CAG.41", "Anaerostipes", "Eubacterium_G")) {
-    print(str_c("Processing feature: ", f))
+# for (f in c("Fusobacterium", "Parvimonas", "Peptostreptococcus", "Porphyromonas", "CAG.41", "Anaerostipes", "Eubacterium_G")) {
+#     print(str_c("Processing feature: ", f))
 
-    tmp <- more_plot_data %>%
-        filter(feature == f)
+#     tmp <- more_plot_data %>%
+#         filter(feature == f)
 
-    plot <- ggplot() +
-        geom_hline(yintercept = 0) +
-        geom_point(data = tmp, aes(x = log10relAb, y = shap_value, color = Condition), alpha = 0.3) +
-        theme_presentation() +
-        ggtitle(f)
+#     plot <- ggplot() +
+#         geom_hline(yintercept = 0) +
+#         geom_point(data = tmp, aes(x = feature_value, y = shap_value, color = Condition), alpha = 0.3) +
+#         theme_presentation() +
+#         ggtitle(f)
 
 
-    ggsave(plot = plot, filename = here('plots', str_c(dataset, "__", mt, "_SHAP_vs_relAb_scatter_", f, ".pdf")), width = 3.75, height = 2.8)
+#     ggsave(plot = plot, filename = here('plots', str_c(dataset, "__", mt, "_SHAP_vs_relAb_scatter_", f, ".pdf")), width = 3.75, height = 2.8)
 
-}
+# }
 
-plot <- ggplot() +
+plot1 <- ggplot() +
     geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-    #    geom_boxplot(data = more_plot_data, aes(x = feature, y = shap_value, fill = Condition), alpha = 0.3) +
-    geom_point(data = more_plot_data %>% filter(log10relAb == pc), aes(x = feature, y = shap_value, fill = Condition, shape = Condition), position = position_jitterdodge(jitter.width = 0.25), color = 'black', alpha = 0.35) +
-    geom_point(data = more_plot_data %>% filter(log10relAb > pc), aes(x = feature, y = shap_value, fill = Condition, shape = Condition, color = log10relAb), position = position_jitterdodge(jitter.width = 0.25), alpha = 0.5) +
+    # geom_boxplot(data = more_plot_data, aes(x = feature, y = shap_value, fill = Condition), alpha = 0.3) +
+    # geom_point(data = more_plot_data %>% filter(feature_value == pc), aes(x = feature, y = shap_value, fill = Condition, shape = Condition), position = position_jitterdodge(jitter.width = 0.25), color = 'black', alpha = 0.35) +
+    # geom_point(data = more_plot_data %>% filter(feature_value > pc), aes(x = feature, y = shap_value, fill = Condition, shape = Condition, color = feature_value), position = position_jitterdodge(jitter.width = 0.25), alpha = 0.5) +
+    geom_point(data = more_plot_data %>%
+        rename(`CAZy copy number` = feature_value) %>%
+        mutate(`CAZy copy number` = `CAZy copy number` + 1), aes(x = feature, y = shap_value, fill = Condition, shape = Condition, color = `CAZy copy number`), position = position_jitterdodge(jitter.width = 0.2), alpha = 0.3) +
     geom_point(data = more_plot_data %>%
         group_by(feature) %>%
         summarize(n = mean(abs(shap_value)) * spearman_sign), aes(x = feature, y = n), shape = 18, color = 'orange', size = 2.5, inherit.aes = F) +
@@ -320,13 +342,68 @@ plot <- ggplot() +
         summarize(n = mean(abs(shap_value)) * spearman_sign), aes(x = feature, y = n), shape = 5, color = 'black', size = 2.5, inherit.aes = F) +
     theme_presentation() +
     coord_flip() +
-    scale_color_continuous(low = "blue", high = "yellow") +
-    scale_shape_manual(values = c("CRC" = 16, "CTR" = 1)) +
-    NULL +
+    scale_color_continuous(low = "blue", high = "yellow", trans = 'log10') +
+    scale_shape_manual(values = c("After fasting" = 16, "Before fasting" = 1)) +
     ylab("SHAP value") +
-    xlab("Genus")
+    xlab("CAZy family")
 
-ggsave(plot = plot, filename = here('plots', str_c(dataset, "_SHAP_vs_relAb_by_CRC_CTR.pdf")), width = 5, height = 4)
+# ggsave(plot = plot1, filename = here('plots', str_c(dataset, "_SHAP_vs_relAb_by_case_control.pdf")), width = 5, height = 8.5)
+
+plot2 <- ggplot() +
+    geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+    geom_boxplot(data = more_plot_data, aes(x = feature, y = shap_value, fill = Condition), alpha = 1) +
+    theme_presentation() +
+    coord_flip() +
+    scale_color_continuous(low = "blue", high = "yellow", trans = 'log10') +
+    scale_shape_manual(values = c("After fasting" = 16, "Before fasting" = 1)) +
+    ylab("SHAP value") +
+    xlab("CAZy family")
+
+subtrate_info_for_plot_3 <- shap_tmp %>%
+    group_by(feature) %>%
+    summarize(n = mean(abs(shap_value))) %>%
+    arrange(desc(n)) %>%
+    left_join(substrate_annotations, by = c("feature" = "Subfamily"))
+
+expand_subtrate_classes_into_matrix <- function(v, families) {
+    # Prep
+    v <- map(v, \(x) str_split(x, ",")[[1]])
+    # Get all unique classes
+    u <- unique(unlist(v))
+    v <- map(v, \(x) data.frame(Seen = as.integer(u %in% x)))
+    v <- do.call('cbind', v)
+    u[is.na(u)] <- "NA"
+    rownames(v) <- u
+    colnames(v) <- families
+    return(v)
+}
+
+data_for_plot_3 <- expand_subtrate_classes_into_matrix(subtrate_info_for_plot_3$FUNCTION_AT_DESTINATION_1, subtrate_info_for_plot_3$feature) %>%
+    rownames_to_column('substrate_class') %>%
+    pivot_longer(-substrate_class) %>%
+    rename() %>%
+    rename(
+        family = name,
+        Substrate = value) %>%
+    mutate(Substrate = Substrate == 1) %>%
+    inner_join(data.frame(feature = unique(more_plot_data$feature)), by = c('family' = 'feature')) %>%
+    mutate(family = factor(family, levels = levels(more_plot_data$feature))) %>%
+    mutate(substrate_class = factor(
+        substrate_class,
+        levels = c(unique(substrate_class)[!unique(substrate_class) %in% c("NA", "Other", "Unknown")], c("NA", "Other", "Unknown"))))
+
+plot3 <- ggplot(data = data_for_plot_3) +
+    geom_tile(aes(x = substrate_class, y = family, fill = Substrate)) +
+    theme_presentation() +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1)
+    ) +
+    ylab("CAZy family") +
+    scale_fill_manual(values = c("TRUE" = "black", "FALSE" = "lightgrey")) +
+    NULL
+
+ggsave(plot = plot1 + plot2 + plot3 + plot_layout(width = c(2, 1.75, 1.5), guides = 'collect'), filename = here('plots', str_c(dataset, "_SHAP_vs_relAb_by_case_control.pdf")), width = 10, height = 8.5)
+
 
 
 models2 <- models %>%
@@ -337,6 +414,7 @@ models2 <- models %>%
     mutate(beta_value = -1 * beta_value) %>%
     inner_join(
         shap %>%
+            ungroup() %>%
             filter(model_type == 'lasso', on == "test\nset") %>%
             select(fold, resampling, on, model_type, shap_values_long) %>%
             unnest(shap_values_long) %>%
