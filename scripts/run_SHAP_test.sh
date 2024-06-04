1dabd943951f8ed54661917adf43b1df69ff940e
karcher <karchernic@gmail.com>
2024-06-04 21:00:03 +0200

    updating slurm files

--- scripts/run_SHAP_test.sh
+++ scripts/run_SHAP_test.sh
@@ -2,7 +2,7 @@ for r_id in 1 2 3 4 5
 do	
 	for f_id in 1 2 3 4 5 6 7 8 9 10
 	do
-		for on_what in training testing
+		for on_what in testing
 			do
 			#for which_model in RF lasso
 			for which_model in RF
