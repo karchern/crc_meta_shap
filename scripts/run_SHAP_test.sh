564ed3b3d3e85f79a5db847bfe99ac0ec28fd605
karcher <karchernic@gmail.com>
2024-06-04 15:23:29 +0200

    adapting run_SHAP_test.sh and slurm scripts

--- scripts/run_SHAP_test.sh
+++ scripts/run_SHAP_test.sh
@@ -1,12 +1,14 @@
 for r_id in 1 2 3 4 5 
 do	
-	for f_id in 1 2 3 4 5
+	for f_id in 1 2 3 4 5 6 7 8 9 10
 	do
 		for on_what in training testing
 			do
-			for which_model in RF lasso
+			#for which_model in RF lasso
+			for which_model in RF
 				do
-				for dataset in Feng Zeller
+				#for dataset in Feng Zeller
+				for dataset in Selin20240604AllData Selin20240604Balanced Selin20240604BalancedAndBlocked
 				do
 					echo -e "Rscript run_SHAP_test.r ${r_id} ${f_id} ${on_what} ${which_model} ${dataset}"
 				done
