7e6fb22aa7b38d44bb0736a40010e21369d4f0b5
karcher <karchernic@gmail.com>
2024-06-06 17:20:32 +0200

    adding missing dataset names to start execution

--- scripts/visualize_SHAP_values.r
+++ scripts/visualize_SHAP_values.r
@@ -8,9 +8,9 @@ library(Matrix)
 source('/g/scb/zeller/karcher/SHAP/scripts/visualize_SHAP_values_utility.r')
 
 for (dataset in c(
-    "Selin20240604Balanced"
-    # "",
-    # ""
+    "Selin20240604AllData"
+    "Selin20240604Balanced",
+    "Selin20240604BalancedAndBlocked"
     )) {
         vis_all(dataset = dataset, label_case = "1", model_types_to_evaluate = c("RF"))
     }
